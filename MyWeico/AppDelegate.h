bbf25aa09b04d4542d49d796630415063e8cfca5
kongfanyi <kongfanyi@kongfanyidemacbook-pro.local>
2016-03-09 09:39:24 +0800

    Initial Commit

---
+++ MyWeico/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeico
+//
+//  Created by kongfanyi on 16/3/9.
+//  Copyright © 2016年 kongfanyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWeico/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWeico
+//
+//  Created by kongfanyi on 16/3/9.
+//  Copyright © 2016年 kongfanyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
